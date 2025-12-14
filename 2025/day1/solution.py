3a6522e16a94ccfe3ea55954a90bf6483f6da301
herboh <chandlerfulmer@gmail.com>
2025-12-14 14:05:02 -0500

    close for part 2 - changing ifs

--- 2025/day1/solution.py
+++ 2025/day1/solution.py
@@ -1,37 +1,28 @@
 lock=list(range(100))
-lines=[]
 current_position=50
-
-def parse_direction(value):
-    direction = value[0]  # 'L' or 'R'
-    number = int(value[1:])  # Extract the numeric part
-    return -number if direction == 'L' else number
-
-with open('input.txt', 'r') as fd:
-    for line in fd:
-        stripped = line.strip()
-        if stripped:
-            lines.append(parse_direction(stripped))
-
-# print(lines)
-# print(lock)
+counter = 0
 
 positions = [current_position]
 times_past_0 = 0
-for line in lines:
-    clicks = abs(line)
-    times_past_0 += clicks // len(lock)  # Full circles
-    if line > 0:
-        if current_position + (clicks % 100) > 100:
-            times_past_0 += 1
-    else:
-        if current_position - (clicks % 100) < 0:
-            times_past_0 += 1
-    current_position = (current_position + line) % 100
-    positions.append(current_position)
-    print(current_position)
 
-print(times_past_0)
+with open('input.txt', 'r') as fd:
+    for line in fd.readlines():
+        number = int(line.replace('L', '-').replace('R', ''))
+        #When R, and move is greater than distance
+        if number > 0 and current_position + number >= len(lock): 
+            times_past_0 = (current_position + number) // len(lock)
+            counter += times_past_0
+        elif number < 0 and current_position + number < 0:
+            times_past_0 = abs((current_position + number ) // len(lock))
+            counter += times_past_0
+        current_position= (current_position+number) % len(lock)
+        # if current_position == 0:
+        #     counter += 1
+        positions.append(current_position)
+        print(current_position)
+
+print("Times landeed on 0:")
 print(positions.count(0))
-final = times_past_0+positions.count(0)
-print(final)
+
+print("Times passed 0:")
+print(counter)
