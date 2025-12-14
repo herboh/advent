8228331f3602f0501d3748944488c764c944a4a0
herboh <chandlerfulmer@gmail.com>
2025-12-14 13:26:38 -0500

    testing 2nd half - about to refactor

--- 2025/day1/solution.py
+++ 2025/day1/solution.py
@@ -17,9 +17,21 @@ with open('input.txt', 'r') as fd:
 # print(lock)
 
 positions = [current_position]
+times_past_0 = 0
 for line in lines:
-    current_position= (current_position+line) % len(lock)
+    clicks = abs(line)
+    times_past_0 += clicks // len(lock)  # Full circles
+    if line > 0:
+        if current_position + (clicks % 100) > 100:
+            times_past_0 += 1
+    else:
+        if current_position - (clicks % 100) < 0:
+            times_past_0 += 1
+    current_position = (current_position + line) % 100
     positions.append(current_position)
     print(current_position)
 
+print(times_past_0)
 print(positions.count(0))
+final = times_past_0+positions.count(0)
+print(final)
