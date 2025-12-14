33826ca5fffaba0c3cc5255f583214320d889a39
herboh <chandlerfulmer@gmail.com>
2025-12-14 16:32:48 -0500

    Unified solution w/ better readability

--- 2025/day2/solution.py
+++ 2025/day2/solution.py
@@ -1,6 +1,7 @@
 import csv
 
 matching_ids = []
+mirrored_ids = []
 
 def repeats(id_str):
     """Check if the ID has any repeating pattern."""
@@ -8,16 +9,15 @@ def repeats(id_str):
 
     # Check patterns of length > 2 and no more than half the number of total digits
     for chunk_size in range(1, length // 2 + 1):
-        # check if str is divisible by chunk size
         if length % chunk_size != 0:
             continue
-        # split
+
+        # split into chunks and check for matching chunks
         chunks = [id_str[i:i+chunk_size] for i in range(0, length, chunk_size)]
-        # check if repeat
         if len(set(chunks)) == 1:
-            matching_ids.append(int(id))
-            return True # Found Pattern and Added to List
-    return False # No pattern
+            return True 
+
+    return False
 
 with open('input.txt', 'r') as f:
     reader = csv.reader(f)
@@ -25,12 +25,18 @@ with open('input.txt', 'r') as f:
         for id_range in row:
             # Create lists of ids for each range
             start, end = map(int, id_range.split('-'))
-            ids = list(range(start, end + 1))
-            # Load each value as a string, split, and compare the halves
-            for id in ids:
+            for id in range(start, end + 1):
                 id_str = str(id)
-                repeats(id_str)
+                if repeats(id_str):
+                    #Part1
+                    mid = len(id_str) // 2
+                    if id_str[:mid] == id_str[mid:]:
+                        mirrored_ids.append(id)
 
-print(f"List of Invalid IDs:{matching_ids}")
-print(f"Sum of Invalid IDs:{sum(matching_ids)}")
+                    #Part 2
+                    matching_ids.append(id)
+
+#print(f"List of Invalid IDs:{matching_ids}")
+print(f"Part2 - Sum of Invalid IDs:{sum(matching_ids)}")
+print(f"Part1 - Sum of Mirror Invalid IDs:{sum(mirrored_ids)}")
 
