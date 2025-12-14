f3ce1effb5f87f1b8397f575f88354311624d0b9
herboh <chandlerfulmer@gmail.com>
2025-12-14 14:43:33 -0500

    Part 1 and Part 2 solved

--- 2025/day1/solution.py
+++ 2025/day1/solution.py
@@ -1,28 +1,30 @@
-lock=list(range(100))
+lock=100
 current_position=50
-counter = 0
 
 positions = [current_position]
-times_past_0 = 0
+total_zero = 0
 
-with open('input.txt', 'r') as fd:
-    for line in fd.readlines():
-        number = int(line.replace('L', '-').replace('R', ''))
-        #When R, and move is greater than distance
-        if number > 0 and current_position + number >= len(lock): 
-            times_past_0 = (current_position + number) // len(lock)
-            counter += times_past_0
-        elif number < 0 and current_position + number < 0:
-            times_past_0 = abs((current_position + number ) // len(lock))
-            counter += times_past_0
-        current_position= (current_position+number) % len(lock)
-        # if current_position == 0:
-        #     counter += 1
-        positions.append(current_position)
-        print(current_position)
+with open('input.txt', 'r') as f:
+    for line in f:
+        move = int(line.replace('L', '-').replace('R', ''))
+        previous_position = current_position
+        current_position += move
+
+        #Solve Part 1
+        dial = current_position % lock
+        positions.append(dial)
+
+        # Solve Part 2
+        clicks_at_zero = 0 
+        if move > 0: 
+            clicks_at_zero = (current_position // lock) - (previous_position // lock)
+        elif move < 0:
+            clicks_at_zero = (previous_position - 1) // lock - (current_position - 1) // lock
+        total_zero += clicks_at_zero
+
 
 print("Times landeed on 0:")
 print(positions.count(0))
 
 print("Times passed 0:")
-print(counter)
+print(total_zero)
